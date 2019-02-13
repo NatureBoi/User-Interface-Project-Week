984e52b31cdaa40b9b819e92aeb0ad52961b0719
NatureBoi <12373359+NatureBoi@users.noreply.github.com>
2019-02-13 14:18:24 -0500

    Desktop Version Done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-// JS goes here
+let nav_btn = document.querySelector(".menu-btn");
+
+let nav_links = document.querySelector(".nav-links");
+
+nav_btn.addEventListener("click", function() {
+  nav_links.classList.toggle("hide");
+  nav_btn.classList.toggle("close");
+});
