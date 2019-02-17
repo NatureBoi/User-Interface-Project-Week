37e5a7594a9a9abc277acc931188d50a2646fbce
NatureBoi <12373359+NatureBoi@users.noreply.github.com>
2019-02-16 23:10:38 -0500

    Animations added using greensock

--- js/index.js
+++ js/index.js
@@ -4,43 +4,13 @@ let nav_links = document.querySelector(".nav-links");
 
 nav_btn.addEventListener("click", function() {
   nav_links.classList.toggle("hide");
+  TweenMax.staggerFrom("a", 0.2, { opacity: 0, scale: 0.5, x: 80 }, 0.2);
   nav_btn.classList.toggle("close");
 });
 
-//Services Tabs
-class TabLinks {
-  constructor(element) {
-    this.element = element;
-    this.data = this.element.dataset.tab;
-    this.tabs = document.querySelectorAll(".link");
+// Animations
+let ft_btn = document.querySelector(".ft-btn");
 
-    let tabText = document.querySelector(".tab-text");
-    this.h2 = tabText.querySelector("h2");
-
-    this.itemElement = document.querySelector(`.timg[data-tab="${this.data}"]`);
-    this.item = new TabItem(this.itemElement);
-
-    this.element.addEventListener("click", this.select.bind(this));
-  }
-  select() {
-    this.tabs.forEach(el => el.classList.remove("active-tab"));
-    this.element.classList.add("active-tab");
-    this.h2.innerHTML = this.element.innerHTML;
-    this.item.select();
-  }
-}
-
-class TabItem {
-  constructor(element) {
-    this.element = element;
-    console.log(this.element);
-  }
-  select() {
-    let items = document.querySelectorAll(".timg");
-    items.forEach(item => item.classList.remove("img-active"));
-    this.element.classList.add("img-active");
-  }
-}
-
-let tabs = document.querySelector(".tab-links");
-tabs.querySelectorAll(".link").forEach(tab => (tab = new TabLinks(tab)));
+ft_btn.addEventListener("click", () => {
+  TweenLite.from(ft_btn, 1, { boxShadow: "#333 2px 2px 20px" });
+});
